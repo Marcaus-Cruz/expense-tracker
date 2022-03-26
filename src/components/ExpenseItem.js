83cc9e986f1247b6dd7e1516c864c453aba872dc
Marcaus-Cruz <marcauscruz@gmail.com>
2022-03-26 00:01:19 -0700

    cleaner expense items

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,20 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
+function ExpenseItem(props) {
+    const month = props.date.toLocaleString('en-US', {month: 'long'});
+    const day = props.date.toLocaleString('en-US', {day: '2-digit'});
+    const year = props.date.getFullYear();
+
   return (
     <div className="expense-item">
-      <div>March 25th 2022</div>
+      <div>
+          <div>{month}</div>
+          <div>{year}</div>
+          <div>{day}</div>
+      </div>
       <div className="expense-item__description">
-        <h2>Udemy React Course</h2>
-        <div className="expense-item__price">$98.10</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
