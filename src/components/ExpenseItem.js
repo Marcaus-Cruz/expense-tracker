07b249ca122ad90d652559508879097bb9d9ee9f
Marcaus-Cruz <marcauscruz@gmail.com>
2022-03-28 16:29:23 -0700

    theme change

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,19 @@
-import './ExpenseItem.css';
+import './css/ExpenseItem.css';
+import './css/Card.css';
 
 import ExpenseDate from './ExpenseDate.js';
+import Card from './Card';
 
 function ExpenseItem(props) {
 
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,10 +1,12 @@
 import ExpenseItem from "./ExpenseItem";
-import './Expenses.css';
+import Card from "./Card";
+
+import './css/Expenses.css';
 
 function Expenses(props) {
 
   return (
-    <div>
+    <Card className="expenses">
       <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
@@ -25,7 +27,7 @@ function Expenses(props) {
         amount={props.items[3].amount}
         date={props.items[3].date}
       />
-    </div>
+    </Card>
   );
 }
 
