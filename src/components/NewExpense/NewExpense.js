429e8a29c198455cf872d39b7d8c487e6d0664e4
Marcaus-Cruz <marcauscruz@gmail.com>
2022-04-06 20:33:33 -0700

    Added hide form capabilities

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,25 +1,48 @@
-import ExpenseForm from './ExpenseForm';
+import { useState } from "react";
 
-import '../css/NewExpense.css';
+import ExpenseForm from "./ExpenseForm";
+
+import "../css/NewExpense.css";
 
 const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
 
-    const storeExpenseHandler = (enteredExpense) => {
-        const expense = {
-            ...enteredExpense,
-            // Placeholder for now --> will not need this once db is connected
-            id: Math.random.toString()
-        };
-        
-        //send to App.js
-        props.onGetExpense(expense);
+  const storeExpenseHandler = (enteredExpense) => {
+    const expense = {
+      ...enteredExpense,
+      // Placeholder for now --> will not need this once db is connected
+      id: Math.random.toString(),
     };
+    //send to App.js
+    props.onGetExpense(expense);
+    setIsEditing(false);
+  };
 
-    return (
-        <div className='new-expense'>
-            <ExpenseForm onStoreExpense={storeExpenseHandler}/>
+  const setEditingHandler = () => {
+    setIsEditing(true);
+  };
+
+  const stopEditingHandler = () => {
+    setIsEditing(false);
+  };
+
+  return (
+    <div className="new-expense">
+      {!isEditing && (
+        <div>
+        {/* TODO: Add editing capabilities to remove */}
+          <button>Edit Expense</button>{" "}
+          <button onClick={setEditingHandler}>Add Expense</button>
         </div>
-    );
+      )}
+      {isEditing && (
+        <ExpenseForm
+          onStoreExpense={storeExpenseHandler}
+          onCancel={stopEditingHandler}
+        />
+      )}
+    </div>
+  );
 };
 
 export default NewExpense;
