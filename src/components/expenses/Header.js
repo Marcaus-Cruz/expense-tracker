5d95883bb4c93181e75faed933624b4e4d2ccc5a
Marcaus-Cruz <marcauscruz@gmail.com>
2022-04-08 17:25:44 -0700

    Added logo, fixed header a bit

--- src/components/expenses/Header.js
+++ src/components/expenses/Header.js
@@ -1,10 +1,14 @@
 import "../css/Header.css";
+import logoUrl from "../UI/favicon.ico";
 
 //Header component for entire page
 function Header() {
   return (
     <div className="header">
-      <h2>Expense Tracker!</h2>
+      <div className="left">
+        <img src={logoUrl} alt="MC logo" style={{ width: "100px" }} />
+        <h2 style={{ color: "white", lineHeight: "50px", padding: "0px 20px" }}>Expense Tracker</h2>
+      </div>
       <div className="right">
         <button>Login</button>
         <button>Register</button>
