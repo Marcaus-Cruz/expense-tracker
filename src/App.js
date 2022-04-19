febb67287dedc8fba60d07415914d21ce1b1892b
Marcaus-Cruz <marcauscruz@gmail.com>
2022-04-19 16:47:32 -0700

    trying to connect db to pull expenses

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import Header from "./components/expenses/Header";
 import Expenses from "./components/expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
-
 
 const db = [
   {
@@ -28,13 +27,76 @@ const db = [
 ];
 
 function App() {
-  const [expenses, setExpenses] = useState(db);
-  const addExpenseHandler = expense => {
-    setExpenses(prevExpenses => {
+  const [expenses, setExpenses] = useState([]);
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const [httpError, setHttpError] = useState(false);
+
+  useEffect(() => {
+    const fetchExpenses = async () => {
+      setIsLoading(true);
+
+      //Grab expenses from db
+      const response = await fetch(
+        "https://exp-track-bdba3-default-rtdb.firebaseio.com/expenses.json"
+      );
+
+      if (response.ok) {
+        throw new Error("Something went wrong when fetching expenses!");
+      }
+
+      //convert response to json format
+      const responseData = await response.json();
+
+      const loadedExpenses = [];
+
+      for (const key in responseData) {
+        loadedExpenses.push({
+          id: key,
+          title: responseData[key].name,
+          amount: responseData[key].amount,
+          date: new Date(
+            responsesData[key].date.year,
+            `${responsesData[key].date.month}`,
+            responseData[key].date.day
+          ),
+        }); //push
+      } //for loop
+
+      setExpenses(loadedExpenses);
+      setIsLoading(false);
+    };
+
+    fetchExpenses().then().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    })
+
+  }, []);//use effect
+
+  if (isLoading) {
+    return (
+      <section>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section>
+        <p>{httpError}</p>
+      </section>
+    );
+  }
+
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
       return [expense, ...prevExpenses];
     });
   };
-  
+
   return (
     <div>
       <Header />
@@ -42,6 +104,6 @@ function App() {
       <Expenses items={expenses} />
     </div>
   );
-}
+}//app
 
 export default App;
--- src/components/expenses/ExpenseDate.js
+++ src/components/expenses/ExpenseDate.js
@@ -7,8 +7,9 @@ function ExpenseDate(props){
     const month = props.date.toLocaleString('en-US', {month: 'long'});
     const day = props.date.toLocaleString('en-US', {day: '2-digit'});
     const year = props.date.getFullYear();
-    return(
 
+    return(
+        
     //return date component
     <div className="expense-date">
           <div className="expense-date__month">{month}</div>
